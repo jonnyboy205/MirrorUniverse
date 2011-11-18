16830b8dd12a853df6837dae6b5c23b730c3161a
Nate Weiss <ndw2114@columbia.edu>
2011-11-18 13:22:47 -0500

    current updates to the searching stuff

--- src/mirroruniverse/g4/AStar.java
+++ src/mirroruniverse/g4/AStar.java
@@ -3,12 +3,15 @@ package mirroruniverse.g4;
 import java.util.ArrayList;
 import java.util.PriorityQueue;
 
+import mirroruniverse.sim.MUMap;
+
 public class AStar {
 
 	private int[][] map1;
 	private int[][] map2;
 	Node root;
 	PriorityQueue<Node> queue;
+	ArrayList<Node> closed;
 	
 	public AStar(int initialX1, int initialY1, int initialX2, int initialY2, int[][] kb_p1, int[][] kb_p2){
 		root = new Node(initialX1, initialY1, initialX2, initialY2, null, 0);
@@ -16,9 +19,25 @@ public class AStar {
 		map2 = kb_p2;
 		queue = new PriorityQueue<Node>();
 		queue.add(root);
+		closed = new ArrayList<Node>();
+		closed.add(root);
 	}
 	
-	
+	public ArrayList<Integer> findPath(){
+		while(!queue.isEmpty() && queue.peek().getValue() != 0){
+			ArrayList<Node> nexts = successors(queue.poll());
+			queue.addAll(nexts);
+			closed.addAll(nexts);
+		}
+		System.out.println("Done");
+		if(queue.isEmpty()){
+			System.out.println("Empty :(");
+			return null;
+		} else {
+			System.out.println("Found :)");
+			return queue.peek().getActionPath();
+		}
+	}
 	
 	// Will generate the possible next moves 
 	private ArrayList<Node> successors(Node n){
@@ -44,23 +63,44 @@ public class AStar {
 				x2 = n.getX2() + xChange;
 				y2 = n.getY2() + yChange;
 				
-				if(map1[x1][y1] == 1){
+				try {
+					if (map1[x1][y1] == 1) {
+						x1 -= xChange;
+						y1 -= yChange;
+					}
+				} catch (ArrayIndexOutOfBoundsException e) {
 					x1 -= xChange;
 					y1 -= yChange;
 				}
-				if(map2[x2][y2] == 1){
+				try {
+					if (map2[x2][y2] == 1) {
+						x2 -= xChange;
+						y2 -= yChange;
+					}
+				} catch (ArrayIndexOutOfBoundsException e) {
 					x2 -= xChange;
 					y2 -= yChange;
 				}
 				
+				// I should make this better...
+				int[] tempArr = {xChange, yChange};
+				int[][] muMapArr = MUMap.aintDToM;
+				for(int i = 0; i < MUMap.aintDToM.length; ++i){
+					int[] subMapArr = muMapArr[i];
+					if(tempArr[0] == subMapArr[0] && tempArr[1] == subMapArr[1]){
+						index = i;
+						break;
+					}
+				}
+				
 				Node toAdd = new Node(x1, y1, x2, y2, n, index);
 				
-				if(!n.equals(toAdd)){
+				if(!n.equals(toAdd) && !closed.contains(toAdd)){
 					nexts.add(toAdd);
 				}
 			}
 		}
 		
-		return null;
+		return nexts;
 	}
 }
--- src/mirroruniverse/g4/Node.java
+++ src/mirroruniverse/g4/Node.java
@@ -49,7 +49,7 @@ public class Node implements Comparable<Node>{
 		y2 = p2Y;
 		
 		// Set the value of this node equal to our heuristic rating for it
-		value = heuristic(x1,y1,x2,y2);
+		value = this.heuristic();
 		
 		if(parent == null){
 			depth = 0;
@@ -62,11 +62,14 @@ public class Node implements Comparable<Node>{
 	}
 
 	//Implement our actual heuristic here, right now just takes the total distance both players are away from the goal
-	private static int heuristic(int p1X, int p1Y, int p2X, int p2Y){
+	private int heuristic(){
 		if(p1ExitX == -1000 || p2ExitX == -1000){
-			return Integer.MAX_VALUE;
+			if(x1 == 4 && y2 == 4){
+				return 0;
+			}
+			return 10000 + depth;//Integer.MAX_VALUE;
 		}
-		return Math.max(Math.abs(p1X - p1ExitX), Math.abs(p1Y - p1ExitY)) + Math.max(Math.abs(p2X - p2ExitX), Math.abs(p2Y - p2ExitY));
+		return Math.max(Math.abs(x1 - p1ExitX), Math.abs(y1 - p1ExitY)) + Math.max(Math.abs(x2 - p2ExitX), Math.abs(y2 - p2ExitY));
 		
 	}
 
@@ -98,11 +101,15 @@ public class Node implements Comparable<Node>{
 	@Override
 	public int compareTo(Node n) {
 		if(value > n.value){
-			return -1;
-		} else if(value < n.value){
 			return 1;
+		} else if(value < n.value){
+			return -1;
 		} else {
 		return 0;
 		}
+	}
+	
+	public String toString(){
+		return "P1: " + x1 + "," + y1 + "  P2: " + x2 + "," + y2 + "  Value: " + value + " Depth: " + depth;
 	}
 }
