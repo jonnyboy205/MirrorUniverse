3ffa33db3e2eb4b38b17661605f29a50e5e74d1e
Jonathan Bourdett <jab2279@columbia.edu>
2011-11-19 19:44:59 -0500

    updated code with RandomPlayer movements after 100 turns, and go directly to exit if you see it

--- src/mirroruniverse/g4/G4Player.java
+++ src/mirroruniverse/g4/G4Player.java
@@ -18,12 +18,46 @@ public class G4Player implements Player {
 
 	private int numPath;
 	private int initialDir;
-	// private int turn;
+	private int turn;
 	private int stepCounter;
 	private int currentDir;
+	
+	private RandomPlayer myRandomPlayer; 
 
 	@Override
 	public int lookAndMove(int[][] aintViewL, int[][] aintViewR) {
+		int[][] aintLocalViewL = new int[ 3 ][ 3 ];
+		int intMid = aintViewL.length / 2;
+		for ( int i = -1; i <= 1; i ++ )
+		{
+			for ( int j = -1; j <= 1; j ++ )
+			{
+				aintLocalViewL[ 1 + j ][ 1 + i ] = aintViewL[ intMid + j ][ intMid + i ];
+				if ( aintLocalViewL[ 1 + j ][ 1 + i ] == 2 )
+				{
+					if ( i == 0 && j == 0 )
+						continue;
+					return MUMap.aintMToD[ j + 1 ][ i + 1 ];
+				}
+			}
+		}
+		
+		intMid = aintViewR.length / 2;
+		int[][] aintLocalViewR = new int[ 3 ][ 3 ];
+		for ( int i = -1; i <= 1; i ++ )
+		{
+			for ( int j = -1; j <= 1; j ++ )
+			{
+				aintLocalViewR[ 1 + j ][ 1 + i ] = aintViewR[ intMid + j ][ intMid + i ];
+				if ( aintViewR[ intMid + j ][ intMid + i ] == 2 )
+				{
+					if ( i == 0 && j == 0 )
+						continue;
+					return MUMap.aintMToD[ j + 1 ][ i + 1 ];
+				}
+			}
+		}
+		
 		if (!started) {
 			initialize(aintViewL);
 		}
@@ -58,14 +92,16 @@ public class G4Player implements Player {
 		initialDir = 2;
 		currentDir = initialDir;
 		stepCounter = 0;
+		turn = 0;
+		myRandomPlayer = new RandomPlayer();
 	}
 
 	// not currently taking into account obstacles
 	public int move(int[][] aintViewL, int[][] aintViewR) {
+		currentDir = getNormalizedDir();
 		if (stepCounter == calcPathSteps()) {
 			/*if(currentDir%2!=0)
 				currentDir+=1;*/
-			//currentDir = getNormalizedDir();
 			
 			currentDir -= 2;
 			if (currentDir <= 0) {
@@ -75,31 +111,37 @@ public class G4Player implements Player {
 			stepCounter = 0;
 		}
 
-		// turn++;
-//		while(!isDirectionCorrect(currentDir, aintViewL)){
-//			currentDir-=1;
-//		}
+		while(!isDirectionCorrect(currentDir, aintViewL)){
+			currentDir-=1;
+			if (currentDir <= 0) {
+				currentDir = 8;
+			}
+		}
 
 		stepCounter++;
+		if (turn > 100)
+			return myRandomPlayer.lookAndMove(aintViewL, aintViewR);
+		turn++;
 		return currentDir;
 	}
 
 	private int getNormalizedDir() {
+		int temp = 0;
 		int mod = numPath%4;
 		if (mod == 0){
-			currentDir = 2;
+			temp = 2;
 		}
 		else if (mod == 1){
-			currentDir = 8;
+			temp = 8;
 		}
 		else if (mod == 2){
-			currentDir = 6;
+			temp = 6;
 		}
 		else if (mod == 3){
-			currentDir = 4;
+			temp = 4;
 		}
 		
-		return 0;
+		return temp;
 	}
 
 	private int calcPathSteps() {
